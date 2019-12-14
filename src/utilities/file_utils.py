4f0570fe1a6931f649da9315a30d3fed5d6882a4
kiranneelam <neelamm@live.nl>
2019-12-14 11:36:37 +0100

    chunks own file

--- src/utilities/file_utils.py
+++ src/utilities/file_utils.py
@@ -1,8 +1,6 @@
 def read_file_contents(file_name, offset=0, length=None):
 	try:
-		if isinstance(file_name, tuple): f = file_name[0]
-		else: f = file_name
-		with open(f, "r") as file:
+		with open(file_name, "r") as file:
 			file.seek(int(offset))
 			contents = file.read(length)
 			return contents
